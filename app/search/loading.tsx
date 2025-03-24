5b8ecf7d23b5382fbfb8dbac876cc50c7710ed0e
muhmmad Nabh <muhammadnabih2020@gmail.com>
2025-03-24 22:15:01 +0200

    feat: add types

--- app/search/loading.tsx
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-  return (
-    <div className="flex items-center justify-center h-full">
-      <div className="animate-spin rounded-full h-5 w-5 border-b-2 border-primary-foreground"></div>
-    </div>
-  );
-}
